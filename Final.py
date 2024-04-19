1e4bfa25131d2d0c9606cafb79d92413f1115b81
HikariShino <164301798+HikariShino@users.noreply.github.com>
2024-04-19 11:37:31 -1000

    Imported a player ship.

--- Final.py
+++ Final.py
@@ -10,11 +10,17 @@ pygame.display.set_caption("Space Invader")
 
 background = (22, 20, 87)
 
+player_image = pygame.image.load('Assets/Player.png')
+player_rect = player_image.get_rect()
+player_image = pygame.transform.scale(player_image, (50, 50))
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    screen.blit(player_image, player_rect)
 
     pygame.display.update()
 
