5a347521e9ab9813614361e0db814a86c70e68d6
HikariShino <164301798+HikariShino@users.noreply.github.com>
2024-04-20 11:39:48 -1000

    Player ship placed in a better position. Player now moves left and right without going offscreen.

--- Final.py
+++ Final.py
@@ -1,27 +1,57 @@
 import pygame
 
+
 pygame.init()
 
+
+fpsClock = pygame.time.Clock()
 screen_width = 400
 screen_height = 600
-
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Invader")
 
 background = (22, 20, 87)
+fps = 60
 
 player_image = pygame.image.load('Assets/Player.png')
-player_rect = player_image.get_rect()
 player_image = pygame.transform.scale(player_image, (50, 50))
+player_image = pygame.transform.rotate(player_image, 180)
+player_rect = player_image.get_rect()
+
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect = player_rect.copy()
+        self.rect.topleft = (self.x, self.y)
+        self.speed = 6
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.move_ip(-self.speed, 0)
+        if keys[pygame.K_RIGHT] and self.rect.right < screen_width:
+            self.rect.move_ip(self.speed, 0)
+
+
+player = Player(175, 520)
+
 
 run = True
 while run:
+    fpsClock.tick(fps)
+    screen.fill(background)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    screen.blit(player_image, player_rect)
+    player.move()
+
+    screen.blit(player_image, player.rect)
 
     pygame.display.update()
+
 
 pygame.quit()
