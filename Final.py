54cc8cad721d0d1a29fb2937cf18a1fe0570d820
HikariShino <164301798+HikariShino@users.noreply.github.com>
2024-04-25 14:51:34 -1000

    Added an alien that comes from top left when start. Still need collision with laser and other things.

--- Final.py
+++ Final.py
@@ -6,7 +6,7 @@ pygame.init()
 
 fpsClock = pygame.time.Clock()
 screen_width = 400
-screen_height = 600
+screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Invader")
 
@@ -19,6 +19,8 @@ player_image = pygame.image.load('Assets/Player.png')
 player_image = pygame.transform.scale(player_image, (50, 50))
 player_image = pygame.transform.rotate(player_image, 180)
 player_rect = player_image.get_rect()
+
+alien1 = pygame.image.load('Assets/Alien-V1.png')
 
 
 class Player:
@@ -46,13 +48,39 @@ class Laser:
         self.rect.y -= self.speed
 
 
-player = Player(175, 520)
+class Alien:
+    def __init__(self, x, y, image):
+        self.image = pygame.transform.scale(image, (25, 25))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 1
+        self.direction = 1
+        self.down_speed = 1
+        self.moving_down = True
+
+    def update(self):
+        if self.moving_down:
+            self.rect.y += self.down_speed
+            if self.rect.top > 0:
+                self.moving_down = False
+        else:
+            self.rect.x += self.speed * self.direction
+            if self.rect.right >= screen_width or self.rect.left <= 0:
+                self.direction *= -1
+                self.rect.y += 20
+
+
+alien1_image = pygame.image.load('Assets/Alien-V1.png')
+alien1 = Alien(0, -50, alien1_image)
+
+player = Player(175, 420)
 lasers = []
 
 run = True
 while run:
     fpsClock.tick(fps)
     screen.fill(background)
+    alien1.update()
+    screen.blit(alien1.image, alien1.rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
